e8c0f499c9a25b35c73a68337249f9c3d1c58dac
Ernesto Angulo <ernestoangulojimenez@gmail.com>
2020-08-23 22:31:10 -0500

    On and emit events

--- server/server.js
+++ server/server.js
@@ -1,33 +1,53 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const http = require('http')
-const socket = require('socket.io')
+const http = require('http');
+const socket = require('socket.io');
 
 //Inicializacion de Express
-const app = express()
+const app = express();
 
 //Creacion de un servidor HTTP
-let server = http.createServer(app)
+let server = http.createServer(app);
 
 //Puerto
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.use('/', express.static(path.resolve(__dirname, '../public')))
+app.use('/', express.static(path.resolve(__dirname, '../public')));
 
 //Comunicacion del backend
-let io = socket(server)
+let io = socket(server);
 
 //Conexion del servidor
 io.on('connection', (client) => {
-  console.log('client connected')
-  
+  console.log('client connected');
+
+  //Enviar informacion
+  client.emit('sendMessage', {
+    user: 'Admin',
+    message: 'Welcome to Sockets',
+  });
+
   //Desconexion del servidor
   client.on('disconnect', () => {
-    console.log('User disconnected')
-  })
-})
+    console.log('client disconnected');
+  });
+
+  //Listen Client
+  client.on('sendMessage', (message, callback) => {
+    console.log(message);
+    if(message.user) {
+      callback({
+        response: 'OK'
+      });
+    } else {
+      callback({
+        response: 'NOOO!!'
+      })
+    }
+  });
+});
 
 server.listen(port, (err) => {
-  if (err) throw new Error(err)
-  console.log(`Listening to port ${port}`)
-})
+  if (err) throw new Error(err);
+  console.log(`Listening to port ${port}`);
+});
